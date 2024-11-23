9d63c133f5fbf7417b6c49c53309f703d5e5b553
Akhil Raj <akhilraaaj7@gmail.com>
2024-11-23 21:50:26 +0530

    style(refractor): update 404 section and code format

--- src/components/content/Page404.jsx
+++ src/components/content/Page404.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 
 const Page404 = () => {
   return (
-    <section className="flex w-full dark:text-gray-100 h-screen">
+    <section className="flex w-full dark:text-gray-100 h-screen bg-pattern">
 	<div className="container flex flex-col items-center justify-center px-5  my-8">
 		<div className="max-w-md text-center">
-			<h2 className="mb-8 font-extrabold text-9xl text-gray-800">404</h2>
-			<p className="text-xl font-extrabold italic md:text-3xl text-red-700 whitespace-nowrap mb-4">Sorry, we couldn't find this page.</p>
-			<Link to="/" className="btn-wide btn  bg-[#2f89b3] btn-accent text-white font-bold border border-blue-900 text-lg mt-4 mb-8">Back to Home Page</Link>
+			<h2 className="mb-8 font-extrabold text-9xl text-white">404</h2>
+			<p className="text-xl font-extrabold italic md:text-3xl text-gray-800 whitespace-nowrap mb-4">Sorry, we couldn't find this page.</p>
+			<Link to="/" className="btn-wide btn bg-slate-800 border-none hover:bg-slate-900 text-white font-bold text-lg mt-4 mb-8">Back to Home Page</Link>
 		</div>
 	</div>
 </section>
